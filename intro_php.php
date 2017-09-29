23e2b5f496ad416715e947d8b5955e4c07ad2128
Amanda Kirchner <geeky.brewster@gmail.com>
2017-09-28 19:14:05 -0500

    Added initial, simple php and html

---
+++ intro_php.php
@@ -0,0 +1,19 @@
+<?php echo 'Hello World from PHP!'; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Dynamic Text</title>
+  </head>
+  <body>
+
+<?php
+  $title = 'What a Wonderful World!';
+  // Hi, I'm a comment.
+  /* And I'm a multi-line
+  comment. Look familiar? */
+?>
+    <h1><?php echo $title; ?></h1>
+
+  </body>
+</html>

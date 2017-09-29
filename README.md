e1ba140cb51c38f398e2d524e83dc5e9136d1f4f
Amanda Kirchner <geeky.brewster@gmail.com>
2017-09-29 11:58:38 -0500

    Updated readme. Added notes on arrays to intro file

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Learning PHP
 
 Collection of files generated while learning PHP, using both a Udemy course (PHP for Beginners) and personal projects
+
+## Technologies Used
+- PHP
+- [MAMP](https://www.mamp.info/en/) to run local Apache server & mySQL database
+- mySQL
--- intro_php.php
+++ intro_php.php
@@ -7,13 +7,33 @@
   </head>
   <body>
 
-<?php
-  $title = 'What a Wonderful World!';
-  // Hi, I'm a comment.
-  /* And I'm a multi-line
-  comment. Look familiar? */
-?>
-    <h1><?php echo $title; ?></h1>
+    <?php
+      $title = 'What a Wonderful World!';
+      $firstName = "Bob";
+      $lastName = "Smith";
+      // Hi, I'm a comment. And above is a variable (denoted w/ $)
+      /* And I'm a multi-line
+      comment. Look familiar? */
+    ?>
 
-  </body>
-</html>
+        <h1><?php echo $title; ?></h1>
+        <p><?php echo 'My name is '.$firstName.' '.$lastName /*concatenated string*/?></p>
+        <p> Arrays are created w/ array() function.</p>
+
+    <?php
+    //Arrays
+    $numberList = array(1,3,'5',7,'9','<em>11</em>');
+    echo '<p>Number list array using echo: '.$numberList.'</p>';
+    echo '<p>Number list array index 0 using echo: '.$numberList[0].'</p>';
+    echo '<p>Number list array using print_r: </p>';
+    print_r($numberList);
+
+    //Associative Arrays
+    $names = array("first_name" => 'Mary', "last_name" => 'Johnson', "city" => 'Madison');
+    echo '<p>Associative array using print_r: </p>';
+    print_r($names);
+    echo '<p>First name is: '.$names['first_name'].'</p>';
+     ?>
+
+      </body>
+    </html>

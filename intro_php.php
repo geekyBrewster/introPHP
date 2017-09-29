10db58725307f9b30d19fec4925f253bf2a136d7
Amanda Kirchner <geeky.brewster@gmail.com>
2017-09-29 14:24:17 -0500

    Added code on conditional statements and loops

--- intro_php.php
+++ intro_php.php
@@ -35,5 +35,59 @@
     echo '<p>First name is: '.$names['first_name'].'</p>';
      ?>
 
+     <hr>
+<p> Conditional Statements</p>
+<?php
+// If Statement
+if (3 < 10){
+echo '3 is less than 10';
+echo '<br />';
+} else {
+echo 'Oops. That is not right.';
+echo '<br />';
+}
+
+// Switch Statement
+$number = 10;
+
+switch($number){
+  case 1:
+  echo "is it 1?";
+    break;
+  case 10:
+  echo "is it 10?";
+    break;
+  case 20:
+  echo "is it 20?";
+    break;
+  default:
+  echo "Don't know. What is it?";
+    break; //not necessary
+}
+echo '<p>The syntax for switch and while are like JS.</p>';
+// While loop
+$counter = 1;
+
+while($counter <= 10){
+echo $counter.', ';
+$counter++;
+}
+
+echo '<p>The syntax for a For loop is too.</p>';
+// For loop
+for($i = 0; $i <= 100; $i+=10){
+echo $i.', ';
+}
+
+echo '<p>Using a Foreach loop on an array.</p>';
+// ForEach loop
+$fruitArray = ['Apple', 'Banana', 'Cantaloupe', 'Durian', 'Kiwi', 'Orange', 'Starfruit'];
+
+foreach($fruitArray as $fruit){
+echo '<p>I like to eat '.$fruit.'!</p>';
+}
+
+?>
+
       </body>
     </html>

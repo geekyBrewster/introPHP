59fc9705b94ff992826e4b0e78fe4739a2ee3732
Amanda Kirchner <geeky.brewster@gmail.com>
2017-10-06 12:50:16 -0500

    Added initial functions

--- functions.php
+++ functions.php
@@ -7,7 +7,23 @@
   <body>
     <h1>Functions in PHP</h1>
 
+<?php
+// Define a function
+function greeting($name)){
+  echo "Hello, ".$name;
+}
+greeting('Joe');
 
+function addNumbers($num1, $num2){
+  $sum = $num1 + $num2;
+  echo $sum
+}
+addNumbers(13, 67);
+
+
+
+
+?>
 
 
 
